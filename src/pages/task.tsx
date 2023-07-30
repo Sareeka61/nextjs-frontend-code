02478b1a5d476d4b603adf6cf245972c4d02e873
sareeka61 <ghimiresarika61@gmail.com>
2023-07-30 13:35:11 +0545

    created input field

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -8,13 +8,34 @@ const fetchTask = async () => {
     return res
 };
 
+const createTask = async (data:any) => {
+    const res = await axios.post(
+        'http://localhost:3000/task', data
+    );
+    return res
+}
+
 export default function TaskPage(){
     const [tasks,setTasks] = useState<any>()
-    
+    const [taskName, setTaskName] = useState("")
+
     useEffect(()=> {
-        fetchTask().then((task)=> setTasks(task))
+        fetchTask().then((task)=> setTasks(task.data))
     }, [])
+
+    const  handleCreateTask = () => {
+        createTask({taskName: taskName, taskStatus: false})
+    }
     return(
-        <div>Task App in NestJS ans NextJS</div>
+        <div>
+            Task App in NestJS and NextJS
+        {
+            tasks && tasks.map((task:any)=>(
+                <div key={task}>{task.taskName}</div>
+            ))
+        }
+        <input value={taskName} onChange={(e)=> setTaskName(e.target.value)}/>
+        <button onClick={handleCreateTask}>Create Task</button>
+        </div>
     )
 }
